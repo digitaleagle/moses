e1adb16f9665004f66da21b06d03ed1cdb55e8a8
Stephen Phillips <stpehen@digitaleagle.net>
2024-01-02 22:51:01 -0500

    demonstrated updating Firestore

--- lib/admin/main_admin_screen.dart
+++ lib/admin/main_admin_screen.dart
@@ -3,7 +3,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class MainAdminScreen extends StatelessWidget {
+class MainAdminScreen extends StatefulWidget {
+  const MainAdminScreen({super.key});
+
+  @override
+  State<MainAdminScreen> createState() => _MainAdminScreenState();
+}
+
+class _MainAdminScreenState extends State<MainAdminScreen> {
+  TextEditingController frontMessageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +26,19 @@ class MainAdminScreen extends StatelessWidget {
             onTap: () {
               context.go("/");
             },
-            leading: Icon(Icons.chevron_left),
-            title: Text("Go Back Home"),
+            leading: const Icon(Icons.chevron_left),
+            title: const Text("Go Back Home"),
           ),
+          TextField(
+            controller: frontMessageController,
+            decoration: const InputDecoration(
+              labelText: "Front Page Message",
+            ),
+          ),
+          ElevatedButton(onPressed: () {
+            var newValue = <String, String>{"message": frontMessageController.text};
+            FirebaseFirestore.instance.collection("font-page").doc("announcements").set(newValue);
+          }, child: const Text("Save Front Message")),
           FutureBuilder<String>(
               future: getUserInfo(),
               builder: (context, snapshot) {
@@ -29,7 +48,7 @@ class MainAdminScreen extends StatelessWidget {
                 if(snapshot.hasError) {
                   return Text("Error!  ${snapshot.error}");
                 }
-                return CircularProgressIndicator();
+                return const CircularProgressIndicator();
               },
           ),
         ],
@@ -38,7 +57,15 @@ class MainAdminScreen extends StatelessWidget {
   }
 
   Future<String> getUserInfo() async {
-    var uid = FirebaseAuth.instance.currentUser!.uid ?? "<no name>";
+    // Front page message
+    final docSnap = await FirebaseFirestore.instance.collection("font-page").doc("announcements").get();
+    var frontPageData = docSnap.data();
+    if(frontPageData != null) {
+      frontMessageController.text = frontPageData["message"];
+    }
+
+    // User Experiments
+    var uid = FirebaseAuth.instance.currentUser!.uid;
     var displayName = FirebaseAuth.instance.currentUser!.displayName ?? "<no name>";
     var userSnapshot = await FirebaseFirestore.instance.collection("user").get();
     var data = userSnapshot.docs;
